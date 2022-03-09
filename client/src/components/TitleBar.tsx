86649ce837bb5d1d4f1be4085910b200899dd889
doyeon <doyeon311@gmail.com>
2022-03-09 22:41:58 +0900

    Refact: 상단바 js -> ts

--- client/src/components/TitleBar.tsx
+++ client/src/components/TitleBar.tsx
@@ -1,44 +1,16 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const TitleContainer = styled.div`
-  width: 100%;
-  height: 4rem;
-  position: sticky;
-  top: 0;
-  display: grid;
-  place-items: center;
-  display: flex;
-  background-color: #0b3961;
-  z-index: 1;
-  color: white;
-`;
-
-const LogoContent = styled.div`
-  align-items: center;
-  font-size: 20px;
-  font-weight: bold;
-  padding: 1rem;
-  flex: 7;
-`;
-const Logo = styled.link`
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Bar = styled.div`
-  float: right;
-  font-size: 12px;
-  z-index: -1;
-  text-align: center;
-  cursor: pointer;
-  margin: 20px;
-  flex: 1;
-  white-space: pre-line;
-`;
-export default function TitleBar({ username, handleLogout, isLogin }) {
+const TitleBar = ({
+  username,
+  handleLogout,
+  isLogin,
+}: {
+  username: string;
+  handleLogout: () => void;
+  isLogin: boolean;
+}) => {
   return (
     <TitleContainer>
       <LogoContent>
@@ -88,4 +60,43 @@ export default function TitleBar({ username, handleLogout, isLogin }) {
       ) : null}
     </TitleContainer>
   );
-}
+};
+
+export default TitleBar;
+
+const TitleContainer = styled.div`
+  width: 100%;
+  height: 4rem;
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  background-color: #0b3961;
+  z-index: 5;
+  color: white;
+`;
+
+const LogoContent = styled.div`
+  align-items: center;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 1rem;
+  flex: 7;
+`;
+
+const Logo = styled.link`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Bar = styled.div`
+  float: right;
+  font-size: 12px;
+  z-index: -1;
+  text-align: center;
+  cursor: pointer;
+  margin: 20px;
+  flex: 1;
+  white-space: pre-line;
+`;
